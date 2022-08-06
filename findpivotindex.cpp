68a4b571d43a96302641e1e6aa9c37557448caea
Prashant Kumar <104191128+pksk2610@users.noreply.github.com>
2022-08-06 14:36:26 +0530

    Add files via upload

---
+++ findpivotindex.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int getPivot(int arr[],int n){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+}
+
+int main()
+{
+   int arr[5]={6,9,14,2,5};
+   cout<<"pivot inddex is "<<getPivot(arr,5)<<endl;
+
+    return 0;
+}
