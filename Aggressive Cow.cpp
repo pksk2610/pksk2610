61b4da5fc874a60ed412ccf8de2cf91ca116a049
Prashant Kumar <104191128+pksk2610@users.noreply.github.com>
2022-08-09 14:07:32 +0530

    Add files via upload

---
+++ Aggressive Cow.cpp
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isPossible(vector<int>&stalls,int k ,int mid){
+    
+    int cowCount=1;
+    int lastPos=stalls[0];
+    for(int i=0;i<stalls.size();i++){
+        if(stalls[i]-lastPos>=mid){
+            cowCount++;
+            if(cowCount==k){
+                return true;
+            }
+            lastPos=stalls[i];
+        }
+    }
+    return false;
+}
+int aggressiveCows(vector<int>&stalls,int k){
+    sort(stalls.begin(),stalls.end());
+    int s=0;
+    int maxi=-1;
+    for(int i=0;i<stalls.size();i++){
+        maxi=max(maxi,stalls[i]);
+    }
+    
+    int e=maxi;
+    
+    int ans=-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(isPossible(stalls,k,mid)){
+            ans=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+
+}
