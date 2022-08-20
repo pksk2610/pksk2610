e0c7e015692f061f6f0d7ca01716d1dde2d5bd90
Prashant Kumar <104191128+pksk2610@users.noreply.github.com>
2022-08-20 15:10:31 +0530

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+#include<vector>
+
+using namespace std;
+
+void selectionSort(vector<int>&arr,int n){
+    for(int i=0;i<n-1;i++){
+        int minIndex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIndex])
+            minIndex=j;
+        }
+        swap(arr[minIndex],arr[i]);
+    }
+}
