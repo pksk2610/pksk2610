3b4f427ad3cc63d108c1a94280895c05671c2808
Prashant Kumar <104191128+pksk2610@users.noreply.github.com>
2022-08-30 02:39:54 +0530

    Add files via upload

---
+++ reverse of String.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+void reverse(char name[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(name[s++],name[e--]);
+    }
+
+}
+int getLength(char name[]){
+    int count=0;
+    for(int i=0;name[i]!='\0';i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    
+    char name[20];
+    cin>>name;
+    cout<<name<<endl;
+    int len=getLength(name);
+    cout<<getLength(name)<<endl;
+    reverse(name,len);
+    cout<<name<<endl;
+    
+}
